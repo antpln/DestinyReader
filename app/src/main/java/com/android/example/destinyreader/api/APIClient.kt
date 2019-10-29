b77092734203274f823cec58b814b7a80c2ed849
antoineplin <38262807+C-Flire@users.noreply.github.com>
2019-10-29 16:19:02 +0100

    API Client

--- app/src/main/java/com/android/example/destinyreader/api/APIClient.kt
+++ app/src/main/java/com/android/example/destinyreader/api/APIClient.kt
@@ -1,22 +1,32 @@
 package com.android.example.destinyreader.api
 
+import retrofit2.Call
 import retrofit2.Retrofit
-
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 
 const val ASSETS_BASE_URL = "https://www.bungie.net"
 
-/*
-class APIClient() {
-    private const val BASE_URL : String = "https://www.bungie.net/Platform/Destiny2/"
+
+    private  val BASE_URL : String = "https://www.bungie.net/Platform/Destiny2/"
 
     private val retrofit = Retrofit.Builder()
-        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
         .baseUrl(BASE_URL)
         .build()
 
     interface DestinyAPIService {
-        @GET(Destiny2/Manifest)
+        @Headers("x-api-key: c1afedf42a994801852af8acf6b355d5")
+
+        @GET("Destiny2/Manifest")
+        fun getManifest():
+                Call<ManifestResponse>
     }
-}*/
+
+object DestinyAPI {
+    val retrofitService : DestinyAPIService by lazy {
+        retrofit.create(DestinyAPIService::class.java)
+    }
+}
---
+++ app/src/main/java/com/android/example/destinyreader/api/ManifestResponse.kt
@@ -0,0 +1,24 @@
+package com.android.example.destinyreader.api
+
+import com.google.gson.annotations.SerializedName
+
+data class ManifestResponse (
+    val version : String,
+    val mobileWorldContentPaths : MobileWorldContentPathhs
+    )
+
+data class MobileWorldContentPathhs (
+    val en : String,
+    val fr : String,
+    val es : String,
+    val de : String,
+    val it : String,
+    val ja : String,
+    val ru : String,
+    val pl : String,
+    val ko : String,
+    @SerializedName("es-mx") val es_mx : String,
+    @SerializedName("pt-br") val pt_br : String,
+    @SerializedName("zh-cht") val zh_cht : String,
+    @SerializedName("zh-chs") val zh_chs : String
+)
